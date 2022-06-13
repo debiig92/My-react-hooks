7cd8e8d8d77f708d43088f18edbab7c72bfe98eb
Gerguevara <gerardoguevara64@gmail.com>
2022-06-12 19:48:22 -0600

    added comment

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -1,32 +1,33 @@
 import { useState } from 'react';
-
+df;
 export const CounterApp = () => {
-
-    const [ state, setCounter] = useState({
-        counter1: 10,
-        counter2: 20,
-        counter3: 30,
-    });
-
-    const { counter1, counter2, counter3 } = state;
+  const [state, setCounter] = useState({
+    counter1: 10,
+    counter2: 20,
+    counter3: 30,
+  });
+  //Casp   cuando el use efect tiene muchos objetos y solo necesitamos actualizar uno
+  const { counter1, counter2, counter3 } = state;
 
   return (
     <>
-        <h1>Counter1: { counter1 }</h1>
-        <h1>Counter2: { counter2 }</h1>
-        <h1>Counter3: { counter3 }</h1>
+      <h1>Counter1: {counter1}</h1>
+      <h1>Counter2: {counter2}</h1>
+      <h1>Counter3: {counter3}</h1>
 
-        <hr />
+      <hr />
 
-        <button 
-            className="btn" 
-            onClick={ 
-                () => setCounter({
-                    ...state,
-                    counter1: counter1 + 1,
-                })
-            }>+1</button>
-    
+      <button
+        className="btn"
+        onClick={() =>
+          setCounter({
+            ...state,
+            counter1: counter1 + 1,
+          })
+        }
+      >
+        +1
+      </button>
     </>
-  )
-}
+  );
+};
