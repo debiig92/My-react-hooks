12caa42bc355c02055598169cf3923b2635ae0f4
Gerguevara <gerardoguevara64@gmail.com>
2022-06-14 14:16:24 -0600

    add comments useEffect, customForm hook

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,29 +1,27 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react';
 
 export const Message = () => {
-    
-    const [coords, setCoords] = useState({ x: 0, y: 0});
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
 
-    useEffect(() => {
-        
-        const onMouseMove = ({ x, y }) => {
-            // const coords = { x, y };
-            setCoords({ x, y })
-        }
+  useEffect(() => {
+    // importante que se crea una funcion de esta forma declarativa para tener accesos a su referencia en memoria y remover el listener
+    const onMouseMove = ({ x, y }) => {
+      // const coords = { x, y };
+      setCoords({ x, y });
+    };
 
-        window.addEventListener( 'mousemove', onMouseMove );
-        
-      return () => {
-        window.removeEventListener( 'mousemove', onMouseMove );
-      }
-    }, []);
-    
-
+    window.addEventListener('mousemove', onMouseMove);
+    // El return funciona como un tipo de ngOnDestroy que se dispara cuando el componente se elimina de el dom
+    // es util para remover eventListeners
+    return () => {
+      window.removeEventListener('mousemove', onMouseMove);
+    };
+  }, []);
 
   return (
     <>
-        <h3>Usuario ya existe</h3>
-        { JSON.stringify( coords ) }
+      <h3>Usuario ya existe</h3>
+      {JSON.stringify(coords)}
     </>
-  )
-}
+  );
+};
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,25 +1,26 @@
 import { useState } from 'react';
 
-export const useForm = ( initialForm = {} ) => {
-  
-    const [ formState, setFormState ] = useState( initialForm );
+export const useForm = (initialForm = {}) => {
+  const [formState, setFormState] = useState(initialForm);
 
-    const onInputChange = ({ target }) => {
-        const { name, value } = target;
-        setFormState({
-            ...formState,
-            [ name ]: value
-        });
-    }
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
-    const onResetForm = () => {
-        setFormState( initialForm );
-    }
+  //   como mantiene en memoria el intial valu puede volver a ser asignado para resetear el formulario
+  const onResetForm = () => {
+    setFormState(initialForm);
+  };
 
-    return {
-        ...formState,
-        formState,
-        onInputChange,
-        onResetForm,
-    }
-}
+  // se retorna con spreda operator para tener acceso dede aqui a las propiedades del objeto form sin necesidad de desestructurar en el padre
+  return {
+    ...formState,
+    formState,
+    onInputChange,
+    onResetForm,
+  };
+};
