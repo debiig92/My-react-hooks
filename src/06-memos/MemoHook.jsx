5fce75ec534aa7c15e178cf556be1a90fcca5ba9
Gerguevara <gerardoguevara64@gmail.com>
2022-06-15 17:14:53 -0600

    added comment useMemo y ReactMemo function

--- src/06-memos/MemoHook.jsx
+++ src/06-memos/MemoHook.jsx
@@ -1,46 +1,41 @@
 import { useMemo, useState } from 'react';
 import { useCounter } from '../hooks';
 
+const heavyStuff = (iterationNumber = 100) => {
+  for (let i = 0; i < iterationNumber; i++) {
+    console.log('Ahí vamos...');
+  }
 
-const heavyStuff = ( iterationNumber = 100 ) => {
-    for( let i = 0; i < iterationNumber; i++ ) {
-        console.log('Ahí vamos...');
-    }
-
-    return `${ iterationNumber } iteraciones realizadas`;
-}
-
-
+  return `${iterationNumber} iteraciones realizadas`;
+};
 
 export const MemoHook = () => {
+  const { counter, increment } = useCounter(4000);
+  const [show, setShow] = useState(true);
+  // similar a use efect pero lo que este hace es manter una referencia en memoria de lo que se le asigna y solamente cambia si
+  // la depedencia que le paso cambiar, si se le pasa una funcion esta debe retornar un valor, si no se le pasa dependencias solo se ejecuta una vez
+  // util para tareas pezadas que pueden ser memorizadas despues de la primera vez de carga
+  const memorizedValue = useMemo(() => heavyStuff(counter), [counter]);
 
-    const { counter, increment } = useCounter( 4000 );
-    const [ show, setShow ] = useState(true);
+  return (
+    <>
+      <h1>
+        Counter: <small>{counter}</small>{' '}
+      </h1>
+      <hr />
 
-    const memorizedValue = useMemo( () => heavyStuff(counter), [counter])
+      <h4>{memorizedValue}</h4>
 
-    return (
-        <>
-            <h1>Counter: <small>{ counter }</small>  </h1>
-            <hr />
+      <button className="btn btn-primary" onClick={() => increment()}>
+        +1
+      </button>
 
-            <h4>{ memorizedValue }</h4>
-
-
-            <button
-                className="btn btn-primary"
-                onClick={ () => increment() }
-            >
-                +1
-            </button>
-
-            <button
-                className="btn btn-outline-primary"
-                onClick={ () => setShow( !show )  }
-            >
-                Show/Hide { JSON.stringify(show) } 
-            </button>
-        
-        </>
-    )
-}
+      <button
+        className="btn btn-outline-primary"
+        onClick={() => setShow(!show)}
+      >
+        Show/Hide {JSON.stringify(show)}
+      </button>
+    </>
+  );
+};
--- src/06-memos/Small.jsx
+++ src/06-memos/Small.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 // import { memo } from 'react';
-
+// memo es un metodo que ayuda a memorizar los componentes, solo se recomienda utilizar si el componente es muy grabde
+// evita que un componente se vuelva a redibujar si su padre tuvo un cambio de state
+//react realmente no hace el redibujado pero lo que si hace es ejecutar  logica que el componente tenga
+// en vite es necesario importar React para que lo reconozca
 export const Small = React.memo(({ value }) => {
+  console.log(' Me volví a dibujar :( ');
 
-    console.log(' Me volví a dibujar :( ');
-
-    return (
-        <small>{ value }</small>
-    )
-})
+  return <small>{value}</small>;
+});
