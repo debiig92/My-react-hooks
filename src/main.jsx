248f0c08d20ca9ec358c938f9997384aa0c59970
debiig92 <deby.guardado@gmail.com>
2022-06-16 17:11:12 -0600

    1 file modified

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 // import { HooksApp } from './HooksApp';
 // import { CounterApp } from './01-useState/CounterApp';
-// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
@@ -18,6 +18,6 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <Padre />
+  <CounterWithCustomHook />
   // </React.StrictMode>
 );
