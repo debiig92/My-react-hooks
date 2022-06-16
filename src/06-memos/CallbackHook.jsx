3bd2c8622d90fcbef9a43f4d86381b258edbd028
Gerguevara <gerardoguevara64@gmail.com>
2022-06-15 18:17:04 -0600

    added comments useCallbackHook

--- src/06-memos/CallbackHook.jsx
+++ src/06-memos/CallbackHook.jsx
@@ -1,34 +1,32 @@
 import { useCallback, useEffect, useState } from 'react';
 import { ShowIncrement } from './ShowIncrement';
 
-
 export const CallbackHook = () => {
+  const [counter, setCounter] = useState(10);
+  //1- use callback es parecido a use memo pero este se utiliza para memorizar pocisione en memoria de funciones
+  //2- retorna una funcion ejecutable, que solo ve vuelce a ejecutar cuando su dependencia cambia, similar a un useEffect
+  //3- si la funcion no se pasa como un callback esta va a memorizar el valor que tiene lo cual no lo que se quiere por eso es mejor que se le pase un callback para que esta conozca el state
 
-    const [counter, setCounter] = useState( 10 );
+  const incrementFather = useCallback((value) => {
+    setCounter((c) => c + value);
+  }, []);
 
-    const incrementFather = useCallback(
-      (value) => {
-        setCounter( (c) => c + value );
-      },
-      [],
-    );
+  //se puede conbinar con el use effect y decir que se llame solo cuando cambia el la funcion padre, porque de
+  // lo contrario crearia un look infinito de cambio lla que la funcion que llama es la misma dependencia que esucha
+  useEffect(() => {
+    // incrementFather();
+  }, [incrementFather]);
 
-    useEffect(() => {
-      // incrementFather();
-    }, [incrementFather])
-    
-    
-    // const incrementFather = () => {
-    //     setCounter( counter + 1);
-    // }
+  // const incrementFather = () => {
+  //     setCounter( counter + 1);
+  // }
 
+  return (
+    <>
+      <h1>useCallback Hook: {counter} </h1>
+      <hr />
 
-    return (
-        <>
-            <h1>useCallback Hook: { counter } </h1>
-            <hr />
-
-            <ShowIncrement increment={ incrementFather } />
-        </>
-    )
-}
+      <ShowIncrement increment={incrementFather} />
+    </>
+  );
+};
--- src/06-memos/ShowIncrement.jsx
+++ src/06-memos/ShowIncrement.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
+// Al pazar como prop una funcion y ser esta llamada esta siempre tiene una posicion de memoria diferente
+// porque su componente padre tuvo una re renderizacion y eso hizo un cambio en la posicion de memoria de la funcion increment y no memoriza una que
+// ya no se ocupa
+export const ShowIncrement = React.memo(({ increment }) => {
+  // nunca es buena practica tenes funciones asi puramente invocadas dentro del componente, siempre con su hook prederiblemente
+  console.log(' Me volví a generar :( ');
 
-export const ShowIncrement = React.memo( ({ increment }) => {
-
-    console.log(' Me volví a generar :( ');
-
-    return (
-        <button
-            className="btn btn-primary"
-            onClick={() => {
-                increment( 5 );
-            }}
-        >
-            Incrementar
-        </button>
-    )
-})
+  return (
+    <button
+      className="btn btn-primary"
+      onClick={() => {
+        increment(5);
+      }}
+    >
+      Incrementar
+    </button>
+  );
+});
