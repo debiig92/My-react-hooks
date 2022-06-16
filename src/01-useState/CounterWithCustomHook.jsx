12fe1add25a473bccffde123e1d43667b8778445
debiig92 <deby.guardado@gmail.com>
2022-06-16 17:12:01 -0600

    1 file modified

--- src/01-useState/CounterWithCustomHook.jsx
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -10,13 +10,13 @@ export const CounterWithCustomHook = () => {
       <hr />
 
       <button onClick={() => increment(2)} className="btn btn-primary">
-        +1
+        +2
       </button>
       <button onClick={reset} className="btn btn-primary">
         Reset
       </button>
       <button onClick={() => decrement(2)} className="btn btn-primary">
-        -1
+        -2
       </button>
     </>
   );
