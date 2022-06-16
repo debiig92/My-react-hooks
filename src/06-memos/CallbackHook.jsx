944a01d57463264a295ca9e95fd69bd21a3a95a6
Gerguevara <gerardoguevara64@gmail.com>
2022-06-15 18:27:43 -0600

    added use callback comment

--- src/06-memos/CallbackHook.jsx
+++ src/06-memos/CallbackHook.jsx
@@ -6,7 +6,7 @@ export const CallbackHook = () => {
   //1- use callback es parecido a use memo pero este se utiliza para memorizar pocisione en memoria de funciones
   //2- retorna una funcion ejecutable, que solo ve vuelce a ejecutar cuando su dependencia cambia, similar a un useEffect
   //3- si la funcion no se pasa como un callback esta va a memorizar el valor que tiene lo cual no lo que se quiere por eso es mejor que se le pase un callback para que esta conozca el state
-
+  //4 el c ahce referencia a su valor actual en el sate
   const incrementFather = useCallback((value) => {
     setCounter((c) => c + value);
   }, []);
